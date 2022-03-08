f6fb8454c502c50381cd7888219ea8417a192820
Tushar Anand <tusharranand@gmail.com>
2022-03-08 16:15:16 +0530

    Rolling the dice using Random

---
+++ snakes_ladders_UC2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace snakes_ladders
+{
+    class simulation
+    {
+        public int Roll = 0;
+        public int Score = 0;
+
+        static int Dice()
+        {
+            return new Random().Next(1, 7);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int Roll = Dice();
+            Console.WriteLine(Roll);
+        }
+    }
+}
