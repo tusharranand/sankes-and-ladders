cec6dace4da38e412af46f400e21079bf6c330b2
Tushar Anand <tusharranand@gmail.com>
2022-03-08 18:45:20 +0530

    effects snakes and ladders on the game added

--- snakes_ladders/Program.cs
+++ snakes_ladders/Program.cs
@@ -2,11 +2,51 @@
 
 namespace snakes_ladders
 {
-    class simulation
+    class Simulation
     {
+        public int Roll = 0;
+        public int Current_Place = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+        public const int NO_PLAY = 0;
+
+        static int Dice()
+        {
+            return new Random().Next(1, 7);
+        }
+
+        static int Check()
+        {
+            return new Random().Next(0, 3);
+        }
+
+        public void Play()
+        {
+            int Roll = Dice();
+            Console.WriteLine("Dice rolled a " + Roll);
+            switch (Check())
+            {
+                case LADDER: 
+                    Current_Place = Current_Place + Roll;
+                    Console.WriteLine("Player climbed a Ladder");
+                    break;
+                case SNAKE: 
+                    Current_Place = Current_Place - Roll;
+                    Console.WriteLine("Player was bit by a Snake");
+                    break;
+                case NO_PLAY:
+                    Console.WriteLine("Player couldn't move");
+                    break;
+            }
+            Console.WriteLine("Current Position of player is: {0} \n", Current_Place);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            Console.WriteLine("Current Position of player is: 0");
+            Simulation start = new Simulation();
+            start.Play();
         }
     }
 }
