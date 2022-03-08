dbac1e538371b09e15216e1078a470d6c3b01fca
Tushar Anand <tusharranand@gmail.com>
2022-03-08 14:42:50 +0530

    Introduction to the first task

---
+++ snakes_ladders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snakes_ladders
+{
+    class simulation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+        }
+    }
+}
