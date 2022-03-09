3c6010c872e5890d55f6c5542ceedd57bf61d826
Tushar Anand <tusharranand@gmail.com>
2022-03-09 13:02:26 +0530

    added number of dice throws, ladder climbs, snake bites and no moves

--- snakes_ladders/Program.cs
+++ snakes_ladders/Program.cs
@@ -5,6 +5,10 @@ namespace snakes_ladders
     class Simulation
     {
         public int Roll = 0;
+        public int Num_of_Rolls = 0;
+        public int Snake_Bites = 0;
+        public int Ladder_Climbs = 0;
+        public int No_Moves = 0;
         public int Current_Place = 0;
         public int Last_Place = 0;
         public const int LADDER = 1;
@@ -13,7 +17,10 @@ namespace snakes_ladders
 
         public int Dice()
         {
-            return new Random().Next(1, 7);
+            Random Rolling = new Random();
+            int Roll = Rolling.Next(1, 7);
+            Num_of_Rolls++;
+            return Roll;
         }
 
         public int Check()
@@ -31,15 +38,18 @@ namespace snakes_ladders
                 {
                     case LADDER:
                         Current_Place = Current_Place + Roll;
+                        Ladder_Climbs++;
                         break;
                     case SNAKE:
                         Current_Place = Current_Place - Roll;
+                        Snake_Bites++;
                         if (Current_Place <= 0)
                             {
                                 Current_Place = 0;
                             }
                         break;
                     case NO_PLAY:
+                        No_Moves++;
                         break;
                 }
                 if (Current_Place > 100)
@@ -50,6 +60,10 @@ namespace snakes_ladders
             }
 
             Console.WriteLine("The player reached 100th position.");
+            Console.WriteLine("Number of times the dice was rolled = {0} \n" +
+                "Number of times player climbed a ladder = {1} \n" +
+                "Number of times the player was bitten by a snake = {2} \n" +
+                "Number of times the player wa frozen in place = {3}", Num_of_Rolls, Ladder_Climbs, Snake_Bites, No_Moves);
         }
 
         static void Main(string[] args)
